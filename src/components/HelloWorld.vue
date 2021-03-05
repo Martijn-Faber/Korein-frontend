56c15f9022c9cd4c6cd120bed3cd110b4e4a3363
Martijn Faber <martijn@fabtronics.nl>
2021-03-05 09:43:29 +0100

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,94 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<div v-if="this.status">
+  <Overlay :overlayActive="state === 'prepare'"></Overlay>
+  <h1 v-if="winner.length > 1">{{ winner }}!</h1>
+  <p>Jullie waren {{ delta.differce }} {{ delta.units }} sneller als {{ loser }}</p>
+  <div>
+    <h4>{{ status[0].name }}</h4>
+    <h1>{{ status[0].time }} secondes</h1>
   </div>
+  <div>
+    <h4>{{ status[1].name }}</h4>
+    <h1>{{ status[1].time }} secondes</h1>
+  </div>
+</div>
 </template>
 
 <script>
+import Overlay from '../components/Overlay';
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  components: {
+    Overlay
+  },
+  data() {
+    return {
+      status: Object,
+      winner: String,
+      loser: String,
+      differce: Object,
+      state: "prepare"
+    }
+  },
+  methods: {
+    getStatus: async function() {
+      let response = await fetch('http://localhost:5000/api/getstatus', {
+        mode: 'cors',
+      });
+
+      if (response.ok) {
+        let json = await response.json();
+        this.status = json
+        this.checkWinner()
+      } else {
+        alert("HTTP-Error: " + response.status);
+      }
+    },
+    checkWinner: function(){
+      if (this.status[0].time < this.status[1].time) {
+        this.winner = `Goed gedaan ${this.status[0].name}`
+        this.loser = this.status[1].name
+      } else if (this.status[1].time < this.status[0].time) {
+        this.winner = `Goed gedaan ${this.status[1].name}`
+        this.loser = this.status[0].name
+      } else if (this.status[0].time === this.status[1].time) {
+        this.winner = "Goed gedaan jullie hebben beide gewonnen!"
+      } else {
+        console.log("Foutje moe kunnen")
+        this.winnen = ""
+      }
+      this.checkDifference()
+    },
+    checkDifference: function() {
+      let difference = Math.abs(this.status[1].time - this.status[0].time)
+
+      console.log(difference)
+      
+      this.delta = {
+        differce: difference.toString(),
+        units: "secondes"
+      }
+
+      if (difference === 1) {
+        this.delta.units = "seconde"
+      } else if (difference < 1) {
+        this.delta.units = "miliseconde"
+        this.delta.differce = Math.round(difference * 1000)
+      } else {
+        this.delta.differce = Math.round(difference * 100)/100
+      }
+
+      this.differce.units
+    }
+  },
+  mounted() {
+    this.getStatus()
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+Overlay {
+    z-index: 100;
 }
 </style>
---
+++ src/components/Overlay.vue
@@ -0,0 +1,54 @@
+<template>
+    <div v-if="overlayActive">
+        <div class="overlay">
+            <img src="../assets/korein-logo.svg" alt="logo" class="overlay-logo">
+            <div class="shape"></div>
+            <h1 class="overlay-text">3</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Overlay",
+    props: {
+        overlayActive: Boolean
+    }
+}
+</script>
+
+<style scoped>
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #fff;
+    width: 100vw;
+    height: 100vh;
+}
+
+.overlay-text {
+    position: absolute;
+    transform: translate(-50%, -100%);
+    top: 50%;
+    left: 50%;
+    transition: all 0.3s;
+
+    font-size: 400px;
+    font-weight: 200;
+}
+
+.shape {
+    background-color: #0086eb;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    width: 500px;
+    height: 500px;
+    border-radius: 37% 63% 65% 35% / 59% 38% 62% 41%;
+    transition: all 0.3s;
+}
+</style>
